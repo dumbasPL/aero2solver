980179305b464027d996517069a4b79d6bb73c7d
nezu <29180158+dumbasPL@users.noreply.github.com>
2023-11-08 13:13:50 +0100

    move to clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,45 @@ use aero2solver::{
     solver::Aero2Solver,
 };
 use anyhow::{anyhow, Result};
-use argh::FromArgs;
-use std::{thread, time::Duration};
+use clap::Parser;
+use std::{path::PathBuf, thread, time::Duration};
 
-#[derive(FromArgs)]
-/// Solve Aero2 captchas automatically.
+fn get_model_path(filename: &str) -> String {
+    let model_path = option_env!("MODEL_PATH").unwrap_or("./model");
+    let mut path = PathBuf::from(model_path);
+    path.push(filename);
+    path.to_str().unwrap().to_string()
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Args {
     /// model configuration file.
-    /// default: "./model/captcha.cfg"
-    #[argh(option, short = 'c', default = "String::from(\"./model/captcha.cfg\")")]
+    #[arg(short = 'c', long, default_value_t = get_model_path("captcha.cfg"))]
     model_cfg: String,
 
     /// model weights file.
-    /// default: "./model/captcha.weights"
-    #[argh(
-        option,
-        short = 'w',
-        default = "String::from(\"./model/captcha.weights\")"
-    )]
+    #[arg(short = 'w', long, default_value_t = get_model_path("captcha.weights"))]
     weights: String,
 
     /// model labels file.
-    /// default: "./model/captcha.names"
-    #[argh(
-        option,
-        short = 'l',
-        default = "String::from(\"./model/captcha.names\")"
-    )]
+    #[arg(short = 'l', long, default_value_t = get_model_path("captcha.names"))]
     labels: String,
 
     /// minimum confidence threshold for captcha detection (0.0 - 1.0).
-    /// default: 0.8
-    #[argh(option, short = 't', default = "0.8")]
+    #[arg(short = 't', long, default_value_t = 0.8)]
     threshold: f32,
 
     /// time to wait after aero2 returns an error (in seconds).
-    /// default: 5.0
-    #[argh(option, default = "5.0")]
+    #[arg(long, default_value_t = 5.0)]
     error_delay: f32,
 
     /// time to wait between checking for new captchas (in seconds).
-    /// default: 10.0
-    #[argh(option, default = "10.0")]
+    #[arg(long, default_value_t = 10.0)]
     check_delay: f32,
 
     /// time to wait after successfully solving a captcha (in seconds).
-    /// default: 3300 (55 minutes)
-    #[argh(option, default = "55.0 * 60.0 // 55 minutes")]
+    #[arg(long, default_value_t = 55.0 * 60.0)]
     solved_delay: f32,
 }
 
@@ -63,7 +55,7 @@ fn main() -> Result<()> {
         check_delay,
         solved_delay,
         threshold,
-    } = argh::from_env();
+    } = Args::parse();
 
     if threshold < 0.0 || threshold > 1.0 {
         return Err(anyhow!("Threshold must be between 0.0 and 1.0"));
